aa57985e895a3e943ad0a13947f000acb831a850
vans163 <vans163>
2016-06-03 19:33:06 -0400

    fixed

--- src/inotify.erl
+++ src/inotify.erl
@@ -3,8 +3,11 @@
 
 -export([init/0, add_watch/3, rm_watch/2, read/1]).
 
-load_nif() -> erlang:load_nif("./c_src/inotify", 0).
-
+load_nif() -> 
+    Path = [_ | _] = code:priv_dir(scv),
+    FullPath = filename:join([Path, "scv_drv"]),
+    erlang:load_nif(FullPath, 0).
+    
 init() -> "NIF library not loaded".
 add_watch(_,_,_) -> "NIF library not loaded".
 rm_watch(_,_) -> "NIF library not loaded".
